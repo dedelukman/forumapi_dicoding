80c72db90210e461f14878626fc4b1191730437c
Dede Lukmanul Hakim <ddlukmanulhakim@gmail.com>
2023-10-08 12:58:27 +0700

    addcomment entity

---
+++ src/Domains/comments/entities/_test/AddComment.test.js
@@ -0,0 +1,41 @@
+const AddComment = require('../AddComment');
+
+describe('a AddComment entities', () => {
+  it('should throw error when payload did not contain needed property', () => {
+    // Arrange
+    const payload = {
+      id: 'lukman',
+      owner: 'lukman',
+    };
+
+    // Action and Assert
+    expect(() => new AddComment(payload)).toThrowError(
+      'ADD_COMMENT.NOT_CONTAIN_NEEDED_PROPERTY'
+    );
+  });
+
+  it('should throw error when payload did not meet data type specification', () => {
+    // Arrange
+    const payload = {
+      content: true,
+    };
+
+    // Action and Assert
+    expect(() => new AddComment(payload)).toThrowError(
+      'ADD_COMMENT.NOT_MEET_DATA_TYPE_SPECIFICATION'
+    );
+  });
+
+  it('should create AddComment object correctly', () => {
+    // Arrange
+    const payload = {
+      content: 'lukman',
+    };
+
+    // Action
+    const { content } = new AddComment(payload);
+
+    // Assert
+    expect(content).toEqual(payload.content);
+  });
+});
