46ce47203b2d9351771bf2fc27306e6044fe9035
Dede Lukmanul Hakim <ddlukmanulhakim@gmail.com>
2023-10-08 14:30:54 +0700

    comment repository

---
+++ src/Domains/comments/CommentRepository.js
@@ -0,0 +1,19 @@
+class CommentRepository {
+  async addComment(addComment) {
+    throw new Error('COMMENT_REPOSITORY.METHOD_NOT_IMPLEMENTED');
+  }
+  async verifyAvailableComment(commentId) {
+    throw new Error('COMMENT_REPOSITORY.METHOD_NOT_IMPLEMENTED');
+  }
+  async getCommentById(commentId) {
+    throw new Error('COMMENT_REPOSITORY.METHOD_NOT_IMPLEMENTED');
+  }
+  async getCommentByThreadId(threadId) {
+    throw new Error('COMMENT_REPOSITORY.METHOD_NOT_IMPLEMENTED');
+  }
+  async deleteComment(commentId) {
+    throw new Error('COMMENT_REPOSITORY.METHOD_NOT_IMPLEMENTED');
+  }
+}
+
+module.exports = CommentRepository;
