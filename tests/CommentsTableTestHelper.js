80395433bc6ad46e030d00081ed088579f526461
Dede Lukmanul Hakim <ddlukmanulhakim@gmail.com>
2023-11-16 22:31:30 +0700

    test helper

---
+++ tests/CommentsTableTestHelper.js
@@ -0,0 +1,44 @@
+/* istanbul ignore file */
+const pool = require('../src/Infrastructures/database/postgres/pool');
+
+const CommentsTableTestHelper = {
+  async addComment({
+    id = 'lukman-comment',
+    threadId = 'lukman-thread',
+    owner = 'lukman',
+    content = 'lukman',
+    date = '2023-11-16T07:00:00.000Z',
+  }) {
+    const query = {
+      text: 'INSERT INTO comments VALUES($1, $2, $3, $4, $5)',
+      values: [id, threadId, owner, content, date],
+    };
+
+    await pool.query(query);
+  },
+
+  async findCommentById(id) {
+    const query = {
+      text: 'SELECT * FROM comments WHERE id = $1',
+      values: [id],
+    };
+
+    const { rows } = await pool.query(query);
+
+    return rows;
+  },
+
+  async deleteCommentById(id) {
+    const query = {
+      text: 'UPDATE comments SET is_deleted=TRUE WHERE id=$1',
+      values: [id],
+    };
+    await pool.query(query);
+  },
+
+  async cleanTable() {
+    await pool.query('DELETE FROM comments WHERE 1=1');
+  },
+};
+
+module.exports = CommentsTableTestHelper;
---
+++ tests/ThreadsTableTestHelper.js
@@ -0,0 +1,35 @@
+/* istanbul ignore file */
+const pool = require('../src/Infrastructures/database/postgres/pool');
+
+const ThreadsTableTestHelper = {
+  async addThread({
+    id = 'lukman-thread',
+    title = 'lukman',
+    body = 'lukman',
+    date = '2023-11-16T07:00:00.000Z',
+    owner = 'lukman',
+  }) {
+    const query = {
+      text: 'INSERT INTO threads VALUES($1, $2, $3, $4, $5)',
+      values: [id, title, body, date, owner],
+    };
+
+    await pool.query(query);
+  },
+
+  async findThreadById(id) {
+    const query = {
+      text: 'SELECT * FROM threads WHERE id = $1',
+      values: [id],
+    };
+
+    const result = await pool.query(query);
+    return result.rows;
+  },
+
+  async cleanTable() {
+    await pool.query('DELETE FROM threads WHERE 1=1');
+  },
+};
+
+module.exports = ThreadsTableTestHelper;
